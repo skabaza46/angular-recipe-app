2cbc9bef96d65d2c479178cb04073c95f5567b35
skabaza <skabaza46@gmail.com>
2023-01-07 19:37:09 -0500

    initial commit.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg"),
+    new Recipe("A Test Recipe2", "This is simply a test2", "https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*"),
+
+  ];
+
+}
