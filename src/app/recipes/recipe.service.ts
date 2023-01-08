f8e6ae1893c34c0e62f73edc9c33ed43932bcfc1
skabaza <skabaza46@gmail.com>
2023-01-08 01:01:32 -0500

    Adding Ingredients to Recipes.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,8 +7,22 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe("A Test Recipe", "This is simply a test", "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg"),
-        new Recipe("A Test Recipe2", "This is simply a test2", "https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*"),
+        new Recipe(
+          "Tasty Schnitzel",
+          "A super-tasty Schnitzel - just awesome!",
+          "https://insanelygoodrecipes.com/wp-content/uploads/2022/03/Homemade-Pork-Schnitzel-with-Cauliflower-and-Lemons.jpg",
+          [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20),
+          ]),
+        new Recipe(
+          "Big Fat Burger",
+          "What else you need to say ?",
+          "https://i.insider.com/5a85feb142e1cc26ea3e9afb?width=600&format=jpeg&auto=webp",
+          [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1),
+          ]),
 
     ];
 
