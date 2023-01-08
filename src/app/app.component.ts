b81227a6ee0a3c55e97e04aadc9940550ff73e36
skabaza <skabaza46@gmail.com>
2023-01-07 19:49:58 -0500

    added navigation with Event Binding and ngIf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng4-complete-guide';
+  title = 'Angular Recipe App';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature:string) {
+    this.loadedFeature = feature;
+
+  }
 }
