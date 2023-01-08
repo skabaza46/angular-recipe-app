fb52ed894288a18eb37aa41c760b57d14cba49c9
skabaza <skabaza46@gmail.com>
2023-01-07 20:24:56 -0500

    Allowing the Users to Add Ingredients to the Shopping List.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -12,5 +12,10 @@ export class ShoppingListComponent {
     new Ingredient("Apples", 5),
     new Ingredient("Tomatoes", 10),
   ];
+
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
   
 }
