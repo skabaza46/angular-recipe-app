3179c65b059f6181752b40ebcc026e51647e47d1
skabaza <skabaza46@gmail.com>
2023-01-21 03:41:27 -0500

    added parogramtic navigation to the edit recipe page.

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent  implements OnInit{
+  id: number;
+
+  editMode: boolean = false;
+
+  constructor(private route: ActivatedRoute) { };
+
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params)=> {
+        this.id = +params['id'];
+
+        // Checking to see if the params has an id
+        // if id is undefined, it will be in new mode
+        this.editMode = params['id'] != null;
+
+      }
+    )
+  }
+
+
 
 }
