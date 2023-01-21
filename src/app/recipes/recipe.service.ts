7b6db351ec8f3e2e74c8412057806d7d57dcabc7
skabaza <skabaza46@gmail.com>
2023-01-21 11:41:33 -0500

    Submitting the Recipe edit form.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,24 +6,25 @@ import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-          "Tasty Schnitzel",
-          "A super-tasty Schnitzel - just awesome!",
-          "https://insanelygoodrecipes.com/wp-content/uploads/2022/03/Homemade-Pork-Schnitzel-with-Cauliflower-and-Lemons.jpg",
-          [
-            new Ingredient('Meat', 1),
-            new Ingredient('French Fries', 20),
-          ]),
-        new Recipe(
-          "Big Fat Burger",
-          "What else you need to say ?",
-          "https://i.insider.com/5a85feb142e1cc26ea3e9afb?width=600&format=jpeg&auto=webp",
-          [
-            new Ingredient('Buns', 2),
-            new Ingredient('Meat', 1),
-          ]),
+  private recipes: Recipe[] = [
+      new Recipe(
+        "Tasty Schnitzel",
+        "A super-tasty Schnitzel - just awesome!",
+        "https://insanelygoodrecipes.com/wp-content/uploads/2022/03/Homemade-Pork-Schnitzel-with-Cauliflower-and-Lemons.jpg",
+        [
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 20),
+        ]),
+      new Recipe(
+        "Big Fat Burger",
+        "What else you need to say ?",
+        "https://i.insider.com/5a85feb142e1cc26ea3e9afb?width=600&format=jpeg&auto=webp",
+        [
+          new Ingredient('Buns', 2),
+          new Ingredient('Meat', 1),
+        ]),
 
     ];
 
@@ -39,8 +40,16 @@ export class RecipeService {
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
-
       this.shoppingListService.addIngredients(ingredients);
+    }
 
+    addRecipe(recipe: Recipe){
+      this.recipes.push(recipe);
+      this.recipesChanged.next(this.recipes.slice())
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe){
+      this.recipes[index] = newRecipe;
+      this.recipesChanged.next(this.recipes.slice())
     }
 }
