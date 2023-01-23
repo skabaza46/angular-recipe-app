ef13a419fe8ba2448c0e5c9bce292941a98ae74d
skabaza <skabaza46@gmail.com>
2023-01-23 06:49:25 -0500

    added routes to feature modules recipes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
-import { AuthGuard } from './auth/auth.guard';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeResolverService } from './recipes/recipies-resolver.service';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/recipes" , pathMatch: "full"},
-  {
-    path: "recipes",
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-    { path: "", component: RecipeStartComponent , resolve: [RecipeResolverService]},
-    { path: "new", component: RecipeEditComponent},
-    { path: ":id", component: RecipeDetailComponent, resolve: [RecipeResolverService]},
-    { path: ":id/edit", component: RecipeEditComponent, resolve: [RecipeResolverService]},
-  ] },
   { path: "shopping-list", component: ShoppingListComponent },
   { path: "auth", component: AuthComponent },
 ]
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 
 @NgModule({
     declarations: [
@@ -18,7 +19,12 @@ import { ReactiveFormsModule } from "@angular/forms";
         RecipeDetailComponent,
         RecipeItemComponent,
     ],
-    imports: [RouterModule, CommonModule, ReactiveFormsModule],
+    imports: [
+        RouterModule,
+        CommonModule,
+        ReactiveFormsModule,
+        RecipesRoutingModule,
+    ],
     exports: [
         RecipeStartComponent,
         RecipeEditComponent,
